30c47940bf5fb76b22a96ff8b4db39bf04abf3dc
Jhoenne Estrecho <jhoenneestrecho12@gmail.com>
2023-10-14 23:39:40 +0800

    fin

--- app/components/NavBar..jsx
+++ app/components/NavBar..jsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
         <nav className="mb-12 flex justify-between">
         <ul className="flex items-center">
                
-                <li className="bg-gray-900 text-white px-3 py-2 rounded-xl m-5 text-m hover:text-blue-400"><a href="https://drive.google.com/file/d/1w3UnnflkBRHdMs6P20Mp5veIfnAlSaOq/view?usp=sharing" target="blank">Resume</a></li>
+                <li className="bg-gray-900 text-white px-3 py-2 rounded-xl m-5 text-m hover:text-blue-400"><a href="https://drive.google.com/file/d/1N5WbkGaFgSXORdBisI3AuDJIc4g2ryF1/view" target="blank">Resume</a></li>
                   <li className="hover:text-blue-400"><a href="https://github.com/Jhoenne5" target="blank"><AiFillGithub style={{width:"40px", height:"40px"}}/></a></li>
             </ul>
             <h1 className="text-2xl font-serif mt-5">Developed with Next & tailwind  </h1>
